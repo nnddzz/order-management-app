18b30e733682b4f26b94d94f3eebab6b6fc435b7
nnddzz <m.indra.vm@gmail.com>
2026-02-02 08:51:44 +0700

    add controlled form to create orders

--- frontend/src/components/OrderList.jsx
+++ frontend/src/components/OrderList.jsx
@@ -1,18 +1,30 @@
 import React, {useState} from "react";
 
 const OrderList = () => {
+
+  const [customer, setCustomer] = useState("");
+  const [total, setTotal] = useState("");
+
   const [orders, setOrders] = useState([
     {id:1, customer:"Budi", total:50000},
     {id:2, customer:"Andi", total:75000},
   ]);
 
   const addOrder = () => {
+    if (!customer || !total){
+      alert("Name and Total must be filled!");
+      return;
+    }
     const newOrder = {
       id:Date.now(),
-      customer:"Customer Baru",
-      total: 100000,
+      customer,
+      total: Number(total),
     };
     setOrders([...orders, newOrder]);
+
+    // reset form
+    setCustomer("");
+    setTotal("");
   };
 
   const deleteOrder = (id) => {
@@ -23,13 +35,29 @@ const OrderList = () => {
   return(
     <div>
       <h2>Order List</h2>
-      <button onClick={addOrder} style={{marginBottom:"10px"}}>Tambah Order</button>
-          
+      
+      <div style={{ marginBottom: "10px" }}>
+        <input
+        type="text"
+        placeholder="Customer Name"
+        value={customer}
+        onChange={(e) => setCustomer(e.target.value)}
+        />        
+
+        <input
+          type="number"
+          placeholder="Total"
+          value={total}
+          onChange={(e) => setTotal(e.target.value)}
+        />
+      </div>
+
+      <button onClick={addOrder} style={{marginBottom:"10px"}}>Add Order</button>          
       <ul>
         {orders.map(order => (
           <li key={order.id}>
             {order.customer} - Rp. {order.total}
-            <button onClick={()=>deleteOrder(order.id)}>Hapus Order</button>
+            <button onClick={()=>deleteOrder(order.id)}>Delete Order</button>
           </li>
         ))}
       </ul>
